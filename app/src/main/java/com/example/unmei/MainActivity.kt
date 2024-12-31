d496bc741af9e178f1b28f7d372679cab3181125
DenisBiushkin <den.biushkin@mail.ru>
2024-12-31 22:34:01 +0300

    Working Firestore

--- app/src/main/java/com/example/unmei/MainActivity.kt
+++ app/src/main/java/com/example/unmei/MainActivity.kt
@@ -11,12 +11,29 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.unmei.domain.model.TestUser
 import com.example.unmei.ui.theme.UnmeiTheme
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : ComponentActivity() {
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val fs=Firebase.firestore
+        fs.collection("TestUser")
+            .document().set(
+                TestUser(
+                    "Denis",
+                    "Biushkin",
+                    20
+                )
+            )
+
         setContent {
             UnmeiTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
---
+++ app/src/main/java/com/example/unmei/domain/model/TestUser.kt
@@ -0,0 +1,8 @@
+package com.example.unmei.domain.model
+
+data class TestUser(
+    val name:String,
+    val secondName:String,
+    val age:Int,
+
+)
