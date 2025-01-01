c0adf2ff9d992787211da6f8bfdcbf803af64519
DenisBiushkin <den.biushkin@mail.ru>
2025-01-01 14:53:10 +0300

    Working FiribaseDatabase

--- app/src/main/java/com/example/unmei/MainActivity.kt
+++ app/src/main/java/com/example/unmei/MainActivity.kt
@@ -13,6 +13,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.unmei.domain.model.TestUser
 import com.example.unmei.ui.theme.UnmeiTheme
+import com.example.unmei.util.ConstansDev
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
@@ -25,14 +27,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         val fs=Firebase.firestore
-        fs.collection("TestUser")
-            .document().set(
-                TestUser(
-                    "Denis",
-                    "Biushkin",
-                    20
-                )
-            )
+
+        val db=FirebaseDatabase.getInstance(ConstansDev.YOUR_URL_DB)
+        val ref = db.getReference("Main_DB")
+        ref.child("Message").setValue("Same Text")
+
+
+//        fs.collection("TestUser")
+//            .document().set(
+//                TestUser(
+//                    "Denis",
+//                    "Biushkin",
+//                    20
+//                )
+//            )
 
         setContent {
             UnmeiTheme {
