b9fe3d1918760f759350fb19067b540da73d0287
DenisBiushkin <den.biushkin@mail.ru>
2025-01-02 19:31:59 +0300

    Почти доделанный экран входа(отрефакторить)

--- app/src/main/java/com/example/unmei/presentation/sign_in/SignInScreen.kt
+++ app/src/main/java/com/example/unmei/presentation/sign_in/SignInScreen.kt
@@ -1,7 +1,10 @@
 package com.example.unmei.presentation.sign_in
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -20,22 +23,33 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.unmei.R
 import com.example.unmei.presentation.util.ui.theme.Black
 import com.example.unmei.presentation.util.ui.theme.BlueGray
 import com.example.unmei.presentation.util.LoginTextField
+import com.example.unmei.presentation.util.SocialMediaLogIn
+import com.example.unmei.presentation.util.socialMedia
 
 @Preview(showBackground = true)
 @Composable
@@ -67,7 +81,7 @@ fun SignInScreen(
                 //.background(Color.Red)
             ) {
                 LoginTextField(
-                    label = "Email",
+                    label = "Почта",
                     trailing = "",
                     modifier = Modifier
                         .fillMaxWidth(),
@@ -78,8 +92,8 @@ fun SignInScreen(
                 )
                 Spacer(modifier = Modifier.height(20.dp))
                 LoginTextField(
-                    label = "Password",
-                    trailing = "Forgot?",
+                    label = "Пароль",
+                    trailing = "Забыли?",
                     modifier = Modifier
                         .fillMaxWidth(),
                     textvalue = passwordTextField.value,
@@ -97,7 +111,7 @@ fun SignInScreen(
                     ),
                     shape = RoundedCornerShape(size = 4.dp), onClick = {
                         //переход по контроллеру с передачей обязательного аргумента
-                       // println(Screen.Main.route)
+                        // println(Screen.Main.route)
                         //println(Screen.Main.passIdandName(10, "Denis"))
 //                        navController
 //                            .navigate(
@@ -108,14 +122,93 @@ fun SignInScreen(
                     }
                 ) {
                     Text(
-                        text = "Log in",
+                        text = "Войти",
                         style = MaterialTheme.typography.labelMedium
                     )
                 }
+
+
+                ///lhkjhkjhkljlkjljjklj
+                Spacer(
+                    modifier = Modifier.height(30.dp)
+                )
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text="Продолжить с",
+                        style=MaterialTheme.typography.labelMedium.copy(color=Color(0xFF64748B))
+                    )
+                    Spacer(modifier = Modifier.height(20.dp))
+                    Row (
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center
+
+                    ){
+                        SocialMediaLogIn(
+                            icon = R.drawable.google,
+                            text = "Google",
+                            OnClick = { /*TODO*/ },
+                            modifier = Modifier.width(100.dp)
+                        )
+                    }
+                }
+                val uiColor = if (isSystemInDarkTheme()) Color.White else Black
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxHeight(0.6f)
+                        .fillMaxWidth()
+                    ,
+                    contentAlignment = Alignment.BottomCenter
+                ){
+                    Row(){
+                        Text(
+                            text= buildAnnotatedString {
+                                withStyle(
+                                    style = SpanStyle(
+                                        color=Color(0xff94A3B8),
+                                        fontSize = 14.sp,
+                                        fontWeight= FontWeight.Normal,
+                                        fontFamily = FontFamily.Default
+                                    )
+                                ){
+                                    append(text = "Нет аккаунта?")
+                                }
+                            }
+                        )
+                        Text(text = buildAnnotatedString {
+                            withStyle(
+                                style = SpanStyle(
+                                    color= uiColor,
+                                    fontSize = 14.sp,
+                                    fontWeight= FontWeight.Medium,
+                                    fontFamily = FontFamily.Default
+                                )
+                            ){
+                                append(text = " Создать..")
+                            }
+                        }
+                        , modifier = Modifier.clickable {
+
+                            })
+                    }
+
+                }
             }
+
         }
 
     }
+}
+
+@Composable
+private fun LoginSection(
+    emailTextField: MutableState<String>,
+    passwordTextField: MutableState<String>
+) {
+
 }
 
 @Composable
@@ -161,9 +254,10 @@ fun TopPart() {
             modifier = Modifier
                 .padding(bottom = 10.dp)
                 .align(alignment = Alignment.BottomCenter)//выровнить
-            , text = stringResource(id = R.string.app_login_text),
+            , text ="Вход",
             style = MaterialTheme.typography.headlineLarge,
             color = uiColor
         )
     }
 }
+
